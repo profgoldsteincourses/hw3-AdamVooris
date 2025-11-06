328c8fadb9d8c87def347089ff3e1448a072b321
AdamVooris <aj03voor@siena.edu>
2025-11-06 17:55:59 -0500

    Fixed Main.py and created a function that makes the user ID

--- main.py
+++ main.py
@@ -1,35 +1,37 @@
 # Add appropriate comments...such as your name, the date...
-
+#Adam Vooris
+#November 16th 2025
+#This code makes a user id with their first and last name and a random number
 import random
 
-# makeID takes two ...
-# and returns...
-def makeID(     ,     ):
+# makeID takes two strings
+# and returns a new id
+def makeID(firstname,lastname):
     # Extract the first inital of their first name
-
+ firstinitial = firstname[0].upper()
     # Extract the first five ...
-
+ last = lastname[:5].upper()
     # Get a random number between 10 and 99
-    myNumber = random.range(   ,   )
+ myNumber = random.randint(10,99)
 
     # Put it all together (concatenate)
+ ID = firstinitial + last + str(myNumber)
+ return ID
 
-    return ID
 
-
-# main prompts the user ....
+# main prompts the user for their first and last name
 def main():
     # Ask for their first name
-    firstName = input("What is your first name? ")
+    firstname = input("What is your first name? ")
 
     # Ask for their last name
-
+    lastname = input("What is your last name? ")
 
     # Call makeID
-    newID =
+    newID = makeID(firstname, lastname)
 
     # Print out the newID
-
+    print("Your new ID is:", newID)
 
 
 # Function definitions above
